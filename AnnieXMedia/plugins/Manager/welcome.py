05f352d1ebd8daebe7d19df1647c3ca75524df54
vzbzbzbzbbxbxbx-cloud <vzbzbzbzbbxbxbx@gmail.com>
2026-02-13 16:13:36 +0600

    welcome.py

--- AnnieXMedia/plugins/Manager/welcome.py
+++ AnnieXMedia/plugins/Manager/welcome.py
@@ -10,7 +10,7 @@ from pyrogram.errors import TopicClosed, PeerIdInvalid, ChannelPrivate, Slowmode
 from AnnieXMedia import app
 from AnnieXMedia.mongo.welcomedb import is_on, set_state, bump, cool, auto_on
 
-BG_PATH = "AnnieXMedia/assets/annie/welcome.png"
+BG_PATH = "AnnieXMedia/assets/annie/welcome2.png"
 FALLBACK_PIC = "AnnieXMedia/assets/upic.png"
 FONT_PATH = "AnnieXMedia/assets/annie/Arimo.ttf"
 
