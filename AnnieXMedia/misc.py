df1403451c7ddd9a909ec405eea9b461e18dac0e
vzbzbzbzbbxbxbx-cloud <vzbzbzbzbbxbxbx@gmail.com>
2026-02-16 15:28:42 +0600

    misc.py
    Removed Heroku integration and related configurations.

--- AnnieXMedia/misc.py
+++ AnnieXMedia/misc.py
@@ -1,12 +1,11 @@
 ﻿# Authored By Certified Coders © 2025
 import socket
 import time
-import heroku3
 
 from pyrogram import filters
 from pyrogram.enums import ChatMemberStatus
 
-from config import HEROKU_API_KEY, HEROKU_APP_NAME, OWNER_ID
+from config import OWNER_ID
 from AnnieXMedia.core.mongo import mongodb
 from .logging import LOGGER
 
@@ -18,11 +17,6 @@ _boot_ = time.time()
 def is_heroku():
     return "heroku" in socket.getfqdn()
 
-XCB = [
-    "/", "@", ".", "com", ":", "git", "heroku", "push",
-    str(HEROKU_API_KEY), "https", str(HEROKU_APP_NAME),
-    "HEAD", "master"
-]
 
 def dbb():
     global db
@@ -48,12 +42,7 @@ async def sudo():
     LOGGER(__name__).info("sᴜᴅᴏ ᴜsᴇʀs ᴅᴏɴᴇ..")
 
 def heroku():
+    # Disabled for Railway / Docker hosting
     global HAPP
-    if is_heroku():
-        if HEROKU_API_KEY and HEROKU_APP_NAME:
-            try:
-                Heroku = heroku3.from_key(HEROKU_API_KEY)
-                HAPP = Heroku.app(HEROKU_APP_NAME)
-                LOGGER(__name__).info("ʜᴇʀᴏᴋᴜ ᴀᴘᴘ ᴄᴏɴғɪɢᴜʀᴇᴅ..")
-            except Exception:
-                LOGGER(__name__).warning("ʏᴏᴜ sʜᴏᴜʟᴅ ʜᴀᴠᴇ ɴᴏᴛ ғɪʟʟᴇᴅ ʜᴇʀᴏᴋᴜ ᴀᴘᴘ ɴᴀᴍᴇ ᴏʀ ᴀᴘɪ ᴋᴇʏ ᴄᴏʀʀᴇᴄᴛʟʏ ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ɪᴛ...")
+    HAPP = None
+    LOGGER(__name__).info("Heroku system disabled (Railway mode)")
