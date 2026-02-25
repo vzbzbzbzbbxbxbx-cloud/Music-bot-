824fac8f9a81f8f158ca9896d85fcd9da431a16d
vzbzbzbzbbxbxbx-cloud <vzbzbzbzbbxbxbx@gmail.com>
2026-02-25 17:00:29 +0600

    Update __init__.py

--- AnnieXMedia/platforms/__init__.py
+++ AnnieXMedia/platforms/__init__.py
@@ -1,8 +1,2 @@
-﻿# Authored By Certified Coders © 2025
-from .Apple import AppleAPI
-from .Carbon import CarbonAPI
-from .Resso import RessoAPI
-from .Soundcloud import SoundAPI
-from .Spotify import SpotifyAPI
-from .Telegram import TeleAPI
 from .Youtube import YouTubeAPI
+from .Telegram import TeleAPI
